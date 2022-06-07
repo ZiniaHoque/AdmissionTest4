72fa9a77508eaa70f6435c5aed5c365f11120325
Zinia Hoque <ziniacse789@gmail.com>
2022-06-07 22:59:34 +0600

    Add project files.

---
+++ Task1/WordCounter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class WordCounter
+    {
+        public int Count(string str)
+        {
+            int count = 0;
+            foreach (var c in str)
+            {
+                if (!char.IsLetter(c))
+                {
+                    count++;
+                }
+            }
+            return count + 1;
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task4/FormatChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class FormatChecker
+    {
+        public bool IsValid(string mobileNumber)
+        {
+            int length = mobileNumber.Length;
+            string code = mobileNumber.Substring(0, 3);
+            if (length == 14 && code == "+88")
+            {
+                return true;
+            }
+            return false;
+            
+            throw new NotImplementedException();
+        }
+    }
+}
